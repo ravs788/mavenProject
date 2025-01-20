b10529563befe014a7c4588af94ec4bcd8c6224f
Ravi Shankar <ravs788@gmail.com>
2025-01-20 06:26:33 +0530

    Added missing testfiles

---
+++ src/test/java/tests/TestCasesEdge.java
@@ -0,0 +1,49 @@
+package tests;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import io.qameta.allure.Description;
+import pages.BaseClass;
+import pages.LoginPage;
+import pages.MainPage;
+
+public class TestCasesEdge {
+	public static WebDriver driver=null;
+	
+	@BeforeSuite
+	public static void setUpFunction(){
+		BaseClass b = new BaseClass();
+		driver = b.setUpFunction("Edge");
+		driver.get("https://www.saucedemo.com");
+	}
+	
+	@Test
+	@Description("This test attempts to login as a standard user")
+	public static void TestCase1() throws InterruptedException{
+		LoginPage loginPage = new LoginPage(driver);
+		MainPage mainPage = new MainPage(driver);
+		
+		Assert.assertTrue(loginPage.logIn("standard_user", "secret_sauce"),"Login successful");
+		Assert.assertTrue(mainPage.logOut(),"Logout successful");
+	}
+	
+	@Test
+	@Description("This test attempts to login as a problem user")
+	public static void TestCase2() throws InterruptedException{
+		LoginPage loginPage = new LoginPage(driver);
+		MainPage mainPage = new MainPage(driver);
+		
+		Assert.assertTrue(loginPage.logIn("problem_user", "secret_sauce"),"Login successful");
+		Assert.assertTrue(mainPage.logOut(),"Logout successful");
+	}
+	
+	@AfterSuite
+	public static void tearDown(){
+		BaseClass b = new BaseClass();
+		b.tearDown(driver);
+	}
+}
